903bb57df4d39bc6106dda58e5dfdf92f24dd2c8
TrinhDucLoc <westudymore@gmail.com>
2022-08-10 11:36:58 +0700

    Update Conditional Rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+
+const allItems = [
+  {id: "beaming", value: "😁", isSmile: true},
+  {id: "grinning", value: "😀", isSmile: true},
+  {id: "squinting", value: "😆", isSmile: true},
+  {id: "winking", value: "😉", isSmile: true},
+  {id: "sad", value: "😥", isSmile: false}
+]
+
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [items, setItems] = useState(allItems)
+  
+  const handleLogin = () => {
+    setIsLoggedIn(true)
+  }
+
+  const handleLogout = () => {
+    setIsLoggedIn(false)
+  }
+
+  const removeItem = (itemID) => {
+    setItems(items.filter((items) => items.id !== itemID))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Smileys</h1>
+      <div className='loginControl'>
+        {isLoggedIn ? (<button onClick={handleLogout}>Login</button>) : (<button onClick={handleLogin}>Logout</button>)}
+      </div>
+      {/* {String(isLoggedIn)} */}
+
+      <div>
+        {isLoggedIn && (
+          <ul>
+            {/* <li>
+              <label>{items[0].value}</label>
+            </li>
+            <li>
+              <label>{items[1].value}</label>
+            </li>
+            <li>
+              <label>{items[2].value}</label>
+            </li> */}
+
+
+            {/* /* Rendering Array */}
+            {items.map((items) => (
+              <li key={items.id}>
+                <button onClick={() => removeItem(items.id)}>Remove</button>
+                <label htmlFor={`${items.id} - input`}>{items.value} {items.id} </label>
+                <input id={`${items.id} - input`} type="text" defaultValue={items.id}/>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
